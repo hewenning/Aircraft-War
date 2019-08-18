cdcab2e846204c4845ca6df0e2e8273372d2fef5
hewenning <919313096@qq.com>
2019-08-18 18:39:14 +0800

    Bullet.set over,and next step will add alien.set.

--- src/Bullet.lua
+++ src/Bullet.lua
@@ -1,8 +1,9 @@
 transition = require "cocos.framework.transition"
 
-bullet = {Tag = 0,}
+bullet = {set = {}, Tag = 0,}
 
-bulletsObj = {}
+
+local i = 1 
 
 Bullet = class("Bullet",function ()
     return cc.Node:create()
@@ -38,23 +39,51 @@ function Bullet:play()
     local action = transition.sequence( 
         {
             cc.MoveBy:create(2, cc.p(0, 1000)), 
-            cc.CallFunc:create( function()
-                self:removeFromParent()
-                print("The bullet has been cleared.")
-            end ),          
+            -- cc.CallFunc:create( function()
+            --     self:removeFromParent()
+            --     print("The bullet has been cleared.")   
+            -- end ),          
         }
     )
     self:runAction(action)
 end
 
+function Bullet:destroy()
+    -- load.hero:addChild(self.node)
+    self:removeFromParent()
+    print("The bullet has been cleared.") 
+end
+
+
 -- 检测空格键或则回车按下的时候 --
 function bullet.update()
     -- print(">>>>>>")
     if keyboard.keyStatus[59] == 1 or keyboard.keyStatus[164] == 1 then
-        local newbullet = Bullet.new(0, 0)
-        newbullet:getBullet()
-        -- print(newbullet.box)
-        newbullet:play()
+        -- local newbullet = Bullet.new(0, 0)
+        -- newbullet:getBullet()
+        -- -- print(newbullet.box)
+        -- newbullet:play()
+        -- 不断的把子弹存入新的Table里面 --
+        if bullet.Tag <= 20 then
+            bullet.set[bullet.Tag] = Bullet.new(0, 0)
+            bullet.set[bullet.Tag]:getBullet()
+            bullet.set[bullet.Tag]:play()
+            bullet.Tag = bullet.Tag + 1
+        elseif bullet.Tag == 21 then
+            if i < 20 then
+                bullet.set[i]:destroy()
+                bullet.set[i] = Bullet.new(0, 0)
+                bullet.set[i]:getBullet()
+                bullet.set[i]:play()
+                i = i + 1
+            elseif i == 20  then
+                bullet.set[i]:destroy()
+                bullet.set[i] = Bullet.new(0, 0)
+                bullet.set[i]:getBullet()
+                bullet.set[i]:play()
+                i = 1
+            end
+        end
     end
 end
 
