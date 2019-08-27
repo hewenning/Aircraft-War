9d475dbfb93bafaf3de972efd27283ec0a93b24a
hewenning <919313096@qq.com>
2019-08-27 15:34:06 +0800

    Add bagview.

--- src/Bag.lua
+++ src/Bag.lua
@@ -1,3 +1,12 @@
 bag = {}
 
+function bag.bagView()
+    local bagViewReader = creator.CreatorReader:createWithFilename('Resources/creator/Scene/bagview.ccreator')
+    local bagScene = bagViewReader:getSceneGraph()
+    local bagNode = bagScene:getChildByName("bag")
+    bagNode:removeFromParent(false)
+    load.Canvas:addChild(bagNode)
+    --bag.propPurchase()
+end
+
 return bag
