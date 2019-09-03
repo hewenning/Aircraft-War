bc0d0a13ff313953c3a7f483c793f45b48880233
hewenning <919313096@qq.com>
2019-09-03 11:11:27 +0800

    Add protobuf.

--- src/Archive.lua
+++ src/Archive.lua
@@ -3,10 +3,42 @@ archive = {}
 function archive.saveGame()
     -- print("Save game.")
     controller.infoDisplay("Game data saved successfully.")
-    cc.UserDefault:getInstance():setIntegerForKey("score", score.value)
-    cc.UserDefault:getInstance():setIntegerForKey("life", heroSet[1]:getLife())
-    cc.UserDefault:getInstance():setIntegerForKey("hp", heroSet[1]:getHP())
-    cc.UserDefault:getInstance():setIntegerForKey("bombnum", prop.bombnum)
+    -- cc.UserDefault:getInstance():setIntegerForKey("score", score.value)
+    -- cc.UserDefault:getInstance():setIntegerForKey("life", heroSet[1]:getLife())
+    -- cc.UserDefault:getInstance():setIntegerForKey("hp", heroSet[1]:getHP())
+    -- cc.UserDefault:getInstance():setIntegerForKey("bombnum", prop.bombnum)
+
+    -- 测试是否能正确使用protobuf --
+    local pbFilePath = cc.FileUtils:getInstance():fullPathForFilename("protobuf/MsgProtocol.pb")
+    release_print("PB file path: "..pbFilePath)
+    
+    local buffer = read_protobuf_file_c(pbFilePath)
+    protobuf.register(buffer) --注:protobuf 是因为在protobuf.lua里面使用module(protobuf)来修改全局名字
+    
+    local stringbuffer = protobuf.encode("Person",      
+        {      
+            name = "Alice",      
+            id = 12345,      
+            phone = {      
+                {      
+                    number = "87654321"      
+                },      
+            }      
+        })           
+    
+    
+    local slen = string.len(stringbuffer)
+    release_print("slen = "..slen)
+    
+    local temp = ""
+    for i=1, slen do
+        temp = temp .. string.format("0xX, ", string.byte(stringbuffer, i))
+    end
+    release_print(temp)
+    local result = protobuf.decode("Person", stringbuffer)
+    release_print("result name: "..result.name)
+    release_print("result name: "..result.id)
+
 end
 
 function archive.loadGame()
@@ -35,7 +67,8 @@ function archive.loadGame()
     local bombNumber = cc.UserDefault:getInstance():getIntegerForKey("bombnum")
     cclog("bombNumber is %d", bombNumber)
     prop.bombnum = bombNumber
-    prop.refreshBombNum() 
+    prop.refreshBombNum()
+ 
 end
 
 return archive
